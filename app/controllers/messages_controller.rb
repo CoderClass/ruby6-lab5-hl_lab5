2ae924eb23397ce0b0d8a7509c51742d02debecf
Lanh Hoang <lanh.hoangcong@gmail.com>
2017-03-15 19:46:40 +0700

    Milestone 2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,24 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:success] = 'Message sent'
+      redirect_to root_path
+    else
+      flash[:error] = 'There is some problem'
+      redirect_to root_path
+    end
+  end
+
+  private
+  # def set_message
+  #   @message = Message.find(params[:id])
+  # end
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
 end
